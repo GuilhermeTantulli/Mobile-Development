935384247fbaaf152f385227779f74a91fa55898
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2025-02-11 11:50:10 -0300

    implemented: Widgets inside Stack

--- aula_1_widgets/lib/main.dart
+++ aula_1_widgets/lib/main.dart
@@ -9,12 +9,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
         ),
+      home: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(color: Colors.red,
+            width: 100,
+            height: 100,
+            ),
+          Container(
+            color: Colors.blue,
+            width: 50,
+            height: 50,
+            ),
+        ],
       ),
     );
   }
