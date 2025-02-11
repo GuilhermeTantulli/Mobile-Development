f802029909bd04125c9ddca12df92325ef53cc4e
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2025-02-11 12:19:39 -0300

    Implemented: Containers & Stacks inside Rows and Columns altogether

--- aula_1_widgets/lib/main.dart
+++ aula_1_widgets/lib/main.dart
@@ -11,22 +11,36 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter',
-      home: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            ),
-          Container(
-            color: Colors.blue,
-            width: 100,
-            height: 100,
-            ),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(color: Colors.red, width: 100, height: 100, ),
+              Container(color: Colors.blue, width: 50, height: 50, ),
+            ],
+          ),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(color: Colors.blue, width: 100, height: 100, ),
+              Container(color: Colors.red, width: 50, height: 50, ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(color: Colors.cyan, height: 50, width: 50, ),
+              Container(color: Colors.pinkAccent, height: 50, width: 50, ),
+              Container(color: Colors.purple, height: 50, width: 50, ),
+            ],
+            )
         ],
       ),
+      
     );
   }
 }
