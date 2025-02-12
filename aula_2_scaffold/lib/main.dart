a92f90d222de09a4767b6e64cb5b3b10962c5ebb
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2025-02-12 10:27:52 -0300

    Implemented: Scaffold Widget

--- aula_2_scaffold/lib/main.dart
+++ aula_2_scaffold/lib/main.dart
@@ -9,11 +9,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('Tarefas'),
         ),
+        body: Container(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){}
+          ),
       ),
     );
   }
